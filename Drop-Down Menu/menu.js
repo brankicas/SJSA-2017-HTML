ef35c826122664e4859f4dcbed0c0d2b84e5cf19
brankicas <33433303+brankicas@users.noreply.github.com>
2017-12-10 20:12:55 +0100

    Add files via upload

---
+++ Drop-Down Menu/menu.js
@@ -0,0 +1,8 @@
+
+
+
+function popUp()
+{
+alert("The if...else if... Statement is an advanced form of if else that allows JavaScript to make a correct decision out of a several conditions");
+	};
+ 
